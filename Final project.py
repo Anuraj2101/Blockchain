8fd1b780eff181b27fb920412f9183618a534f7c
Anuraj2101 <165621778+Anuraj2101@users.noreply.github.com>
2024-04-04 20:44:32 -0400

    Add files via upload (#1)
    Co-authored-by: AlexUOIT <166081850+AlexUOIT@users.noreply.github.com>

---
+++ Final project.py
@@ -0,0 +1,41 @@
+import hashlib
+import datetime
+import random
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return hashlib.sha256((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)).encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.stakeholders = {}
+    
+    def create_genesis_block(self):
+        return Block(0, datetime.datetime.now(), "Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def proof_of_stake(self, wallet_address):
+        if wallet_address in self.stakeholders:
+            return self.stakeholders[wallet_address]
+        else:
+            return 0
+
+    def vote_for_owner(self, wallet_address):
+        self.stakeholders[wallet_address] = self.stakeholders.get(wallet_address, 0) + 1
+
+
